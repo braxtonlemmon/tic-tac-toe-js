6425d9e275f2ef9506f1fba295b9f2525fd61839
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-11-27 15:16:33 +0100

    Allow players to alternate turns. Write function to check for winning combination.

--- game.js
+++ game.js
@@ -11,8 +11,16 @@
 			player
 */
 
-
-
+/*
+	================
+	PLAYER factory
+	================
+*/
+const Player = (name, piece) => {
+	let score = 0;
+	let turn = 0;
+	return { name, piece, score, turn };
+};
 
 
 /*
@@ -22,8 +30,8 @@
 */
 const Gameboard = (function () {
 	// Holds the values of each gameboard spot in an array
-	let gameboard = ['x', 'x', 'x', 'o', 'x', 'x', 'x', 'x', 'x'];
-
+	let gameboard = Array(9).fill('');
+	
 	// Checks to see if board is full
 	function isFull() {
 		return Gameboard.gameboard.every(spot => spot != '') ? true : false;
@@ -57,51 +65,91 @@ const Display = (function () {
 	function addPieceToBoard(e) {
 		const key = e.target.dataset.key;
 		const div = document.querySelector(`.spot[data-key="${key}"]`);
-		if (Gameboard.spotEmpty(div)) div.textContent = 'x';
+		if (Gameboard.spotEmpty(div)) {
+			const current = Game.swapCurrentPlayer();
+			div.textContent = current.piece;
+			Gameboard.gameboard[key] = current.piece;
+		} 
+		
+
+	}
+
+	// Setup names on board 
+	function updateNames() {
+		for (let i = 1; i < 3; i++) {
+			const player = document.querySelector(`.player${i}-name`);
+			const name = document.getElementById(`player${i}`).value;
+			player.textContent = name;
+		}
 	}
 
 	// Updates player scores
 	function updateScores() {
 		for (let i = 1; i < 3; i++) {
 			const player = document.querySelector(`.player${i}`);
-			player.lastElementChild.textContent = players[i - 1].score;
+			player.lastElementChild.textContent = Game.players[i - 1].score;
 		};
 	}
 
-
-	return { gameboardToHTML, addPieceToBoard, updateScores }
+	return { gameboardToHTML, addPieceToBoard, updateScores, updateNames }
 })();
+
+
+
 
 /*
 	==============
 	GAME module
 	==============
 */
+
+
 const Game = (function () {
-	let _currentPlayer;
+	const players = [Player('Braxton', 'x'), Player('Dani', 'o')];
 
-	/*
-	0, 1, 2
-	3, 4, 5
-	6, 7, 8
-	0, 3, 6
-	1, 4, 7
-	2, 5, 8
-	0, 4, 8
-	2, 4, 6
+	// Winning combo possibilities held in array 
+	const winningMoves = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+		[1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+	];
 
-	*/
+	// Sets up and initiates the game
+	function start() {
+		Display.updateNames();
+		Display.updateScores();
+	}
+
+	// Checks for a winning combo on board
+	function checkForWin() {
+		let win = false;
+		winningMoves.forEach(line => {
+			let boardLine = [];
+			line.forEach(spot => boardLine.push(Gameboard.gameboard[spot]));
+			if (boardLine.every(spot => (spot === boardLine[0]) && (spot != ''))) win = true;
+		});
+		return win;
+	};
+
+	// Checks for tie
+	function checkForTie() {
+		return Gameboard.isFull() && !checkForWin() ? true : false;
+	}
+
+	// Returns current player
+	function swapCurrentPlayer() {
+		if (players[0].turn) {
+			players[0].turn = 0;
+			players[1].turn = 1;
+		} else {
+			players[0].turn = 1;
+			players[1].turn = 0;
+		}
+		return players[0].turn ? players[0] : players[1];
+	}
+
+	return { checkForWin, checkForTie, start, players, swapCurrentPlayer };
 })();
 
-/*
-	================
-	PLAYER factory
-	================
-*/
-const Player = (name, piece) => {
-	let score = 0;
-	return { name, piece, score };
-};
 
 
 
@@ -123,11 +171,7 @@ start.addEventListener('click', (e) => {
 	startDiv.style.visibility = 'hidden';
 	const mainDivs = document.querySelectorAll('.main');
 	mainDivs.forEach(div => div.style.opacity = 1);
+	Game.start();
 })
 
 
-
-
-
-// Game execution
-const players = [Player('Braxton', 'x'), Player('Dani', 'o')];
